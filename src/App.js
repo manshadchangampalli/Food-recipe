c9339604b9ee0c14ddb87b93c502439c0962c9e4
manshad <manshadchangampalli010@gmail.com>
2021-05-26 22:41:06 +0530

    the react has been downloaded and creat a component folder and .....

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Menu from './components/Menu/Manu'
+
+function App (){
+
+  return(
+    <div>
+        <Menu/>
+    </div>
+  )
+}
+export default App
---
+++ src/components/Menu/Manu.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import 'Menu.css'
+
+function Menu(){
+    return(
+        <div>
+            <h1>hello</h1>
+        </div>
+    )
+}
+export default Menu
