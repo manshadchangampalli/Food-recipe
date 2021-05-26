faf7045b4a535ba31e7d4a9ce968c702ee0e621a
manshad <manshadchangampalli010@gmail.com>
2021-05-26 23:01:44 +0530

    the api has been called

--- src/components/Menu/Manu.js
+++ src/components/Menu/Manu.js
@@ -1,7 +1,12 @@
-import React from 'react'
-import 'Menu.css'
+import React,{useState,useEffect} from 'react'
+import './Menu.css'
 
 function Menu(){
+    useEffect(async()=>{
+        const API_URL="www.themealdb.com/api/json/v1/1/search.php?f=a";
+        let response= await fetch(API_URL)
+        let data = await response.json()
+    },[])
     return(
         <div>
             <h1>hello</h1>
