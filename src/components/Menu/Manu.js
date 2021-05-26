16f965370a90926cd680654fb17e0effb9991f32
manshad <manshadchangampalli010@gmail.com>
2021-05-26 23:37:55 +0530

    the API call is almost finished

--- src/components/Menu/Manu.js
+++ src/components/Menu/Manu.js
@@ -2,14 +2,32 @@ import React,{useState,useEffect} from 'react'
 import './Menu.css'
 
 function Menu(){
-    useEffect(async()=>{
-        const API_URL="www.themealdb.com/api/json/v1/1/search.php?f=a";
+    const[menu,setMenu]=useState([])
+
+    async function getAllDatas(){
+        const API_URL="https://www.themealdb.com/api/json/v1/1/search.php?f=a";
         let response= await fetch(API_URL)
         let data = await response.json()
+        console.log(data.meals);
+        setMenu(data.meals)
+    }
+
+
+
+        let items=menu.map((items)=>{
+            return(
+                <img src={items.strMealThumb} alt="" />
+            )
+        })
+    
+
+
+    useEffect(()=>{
+        getAllDatas()
     },[])
     return(
-        <div>
-            <h1>hello</h1>
+        <div className="menu_main_div">
+            {items}
         </div>
     )
 }
