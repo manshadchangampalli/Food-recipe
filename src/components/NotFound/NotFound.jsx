8f4497ff2248359568b956e5512e57038fa0ac0d
manshad <manshadchangampalli010@gmail.com>
2021-06-02 23:13:28 +0530

    ...

---
+++ src/components/NotFound/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './NotFound.css'
+
+function NotFound() {
+    return (
+        <div className="notfound">
+            <h3>Sorry The Category is Empty</h3>
+            <h1>Please Choose Another Category</h1>
+        </div>
+    )
+}
+
+export default NotFound
