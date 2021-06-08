67d67504d89e33c21dd0c29df2794ea45933af55
manshad <manshadchangampalli010@gmail.com>
2021-06-09 02:30:47 +0530

    add the popup page some styles and the offer and price tag namediscription country all

--- src/components/Popup/Popup.js
+++ src/components/Popup/Popup.js
@@ -1,22 +1,45 @@
 import React from "react";
 import "./Popup.css";
-import Close from "../icons/Icons"
+import Close from "../icons/Icons";
 
 function Popup(props) {
+
+
+
+
+  let priceByOffer = props.price / 2;
+
 
   return (
     <div className="mainPopup">
       <div className="popup">
-        <div onClick={()=>props.setShowPopup(false)} className="close-icon">
-         <Close/>
+        <div className="priceTag">
+          <p className="strikeprice">${props.price}</p>
+          <p className="priceByOffer">${priceByOffer}</p>
+        </div>
+        <div onClick={() => props.setShowPopup(false)} className="close-icon">
+          <Close />
         </div>
         <div className="popup-div">
           <div className="popup-img">
+            <div className="popup-title">
+              <h1>{props.title.toUpperCase()}</h1>
+            </div>
             <img src={props.img} alt="" />
           </div>
-          <div style={{background:"url("+ props.img +")"}} className="popup-details">
+          <div
+            style={{ background: "url(" + props.img + ")" }}
+            className="popup-details"
+          >
             <div className="for-blur">
-              <h1 className="popup-title">{props.title}</h1>
+              <div className="popupcontent">
+                <li >Its an {props.area} Dish</li>
+                <li>
+                  {props.title} is one of {props.category} itmes
+                </li>
+                <p>{props.desc.length<500?props.desc : props.desc.slice(0,499)+" ..." }<span className="viewmore">{props.desc.length>600&& "view more"}</span></p>
+                
+              </div>
             </div>
           </div>
         </div>
--- src/components/specialDishes/SpecialDishes.jsx
+++ src/components/specialDishes/SpecialDishes.jsx
@@ -8,11 +8,11 @@ function SpecialDishes (props){
     const[popupDetailsState,setPopupDetailsState]=useState({})
     const[showPopup,setShowPopup ]=useState(false)
     let maxNumberOfSpecial=8;
-    function showPopupHandler(img,title,category,area){
+    function showPopupHandler(img,title,category,area,id,description){
         setShowPopup(true)
-         
-        
-        setPopupDetailsState( {img:img,title:title,category:category,area:area} )
+         let price=id/100
+        console.log(price);
+        setPopupDetailsState( {img:img,title:title,category:category,area:area,price:price,desc:description} )
         
     }
     return(
@@ -31,9 +31,11 @@ function SpecialDishes (props){
 
                     props.card_details.map((items,index)=>{
                         if(maxNumberOfSpecial>index){
-                            
+                            console.log(items.strInstructions, items.strIngredient1);
                             return(
-                                <a href="javascript:;" onClick={()=>showPopupHandler(items.strMealThumb,items.strMeal,items.strCategory,items.strArea)}>
+                                <a href="javascript:;"
+                                 onClick={()=>
+                                 showPopupHandler(items.strMealThumb,items.strMeal,items.strCategory,items.strArea,items.idMeal,items.strInstructions)}>
                                <Card strMealThumb={items.strMealThumb} strMeal={items.strMeal} />
                                 </a>
                             )
